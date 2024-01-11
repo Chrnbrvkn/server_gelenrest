0b28a5b4eea6a2bb41734925681eea3042e7cc7a
Nikita <proshaiwow@gmail.com>
2024-01-11 05:13:04 +0400

    cors

--- src/server.js
+++ src/server.js
@@ -1,48 +1,44 @@
-require('dotenv').config()
+require('dotenv').config();
 const express = require('express');
 const sequelize = require('./db');
 const router = require('./routes');
-const cors = require('cors')
-const path = require('path')
-const fs = require('fs');
+const cors = require('cors');
 
-const PORT = process.env.PORT || 8080
-const app = express()
+const PORT = process.env.PORT || 8080;
+const app = express();
 
 app.use('/public/uploads', express.static('public/uploads'));
 
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE');
-  res.header('Access-Control-Allow-Headers', 'Content-Type');
-  res.header('Access-Control-Allow-Credentials', true);
-  next();
-});
-
+const allowedOrigins = ['https://gelenrest.ru', 'http://192.168.0.103:5173'];
 
 const corsOptions = {
-  origin: 'https://gelenrest.ru',
+  origin: (origin, callback) => {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
   credentials: true,
-  optionsSuccessStatus: 200
+  optionsSuccessStatus: 204
 };
 
 app.use(cors(corsOptions));
 
-app.use(express.json())
-app.use('/api', router)
+app.use(express.json());
+app.use('/api', router);
 
 const start = async () => {
   try {
-    await sequelize.authenticate()
-    await sequelize.sync({ alter: true })
+    await sequelize.authenticate();
+    await sequelize.sync({ alter: true });
     app.listen(PORT, () => {
       console.log(`Server is running on http://localhost:${PORT}`);
     });
-
   } catch (e) {
     console.log(e);
   }
-}
+};
 
-start()
+start();
