fb1b0deba6494f37ca3d30a19001874967315800
Nikita <proshaiwow@gmail.com>
2024-01-13 04:45:35 +0400

    fix

--- src/server.js
+++ src/server.js
@@ -9,7 +9,7 @@ const app = express();
 
 app.use('/public/uploads', express.static('public/uploads'));
 
-const allowedOrigins = ['https://gelenrest.ru','https://www.gelenrest.ru','https://localhost:5173'];
+const allowedOrigins = ['https://gelenrest.ru','https://www.gelenrest.ru','https://localhost:5173', 'https://localhost:5174'];
 
 const corsOptions = {
   origin: (origin, callback) => {
