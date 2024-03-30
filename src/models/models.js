7bd776ebe842cabdc909a7d52a6a5b860878bfeb
Nikita <proshaiwow@gmail.com>
2024-03-30 05:12:00 +0400

    models fix

--- src/models/models.js
+++ src/models/models.js
@@ -154,6 +154,7 @@ const Bookings = sequelize.define('booking', {
   itemId: { type: DataTypes.INTEGER, allowNull: false },
   address: { type: DataTypes.STRING, allowNull: false },
   itemType: { type: DataTypes.STRING, allowNull: false },
+  houseId: { type: DataTypes.STRING, allowNull: false },
   houseName: { type: DataTypes.STRING, allowNull: false, defaultValue: '' },
   itemName: { type: DataTypes.STRING, allowNull: false },
   dailyRate: { type: DataTypes.DECIMAL, allowNull: false },
